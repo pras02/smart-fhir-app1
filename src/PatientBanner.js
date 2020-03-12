55a7fabd223edf3bc4678c70e46a4a1f79e8c1a4
Prashanth Prakasha <52795849+pras02@users.noreply.github.com>
2020-03-12 16:14:23 +0530

    Add files via upload

---
+++ src/PatientBanner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PatientBanner = (props) => {
+    return (
+        <div className="banner">
+            <h1>Patient Name: </h1>
+            <h5>Gender: </h5>
+            <h5>Date of Birth: </h5>
+            <h4>MRN:</h4>
+        </div>
+    )
+}
+
+export default PatientBanner
